c2eab793e83e77fc39ea186030a3b78912faae8f
gabryus <gabrielcabralv@hotmail.com>
2023-04-06 20:22:26 -0300

    Update movimento-circular.py

--- exemplos/movimento-circular.py
+++ exemplos/movimento-circular.py
@@ -11,7 +11,7 @@ dt=0.01
 chao = box(pos=vector(0,-2,0), size=vector(20,1,20))
 bola = sphere(pos=vector(r,0,0), color=color.red, make_trail=True)
 
-scene.camera.follow(bola) #camera acompanha a bola
+scene.camera.follow(bola)
 
 while True:
     rate(100)
